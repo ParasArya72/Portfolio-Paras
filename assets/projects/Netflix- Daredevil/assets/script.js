10b0ebfd6c922ab49189dd75fbd301b2d442a044
Paras Arya <parasarya72@gmail.com>
2024-07-02 17:13:04 +0530

    Delete assets directory

--- assets/projects/Netflix- Daredevil/assets/script.js
+++
@@ -1,8 +0,0 @@
-function toggleVideo(){
-    const trailer = document.querySelector('.trailer');
-    trailer.classList.toggle('active');
-    
-    const video = document.querySelector('video');
-    video.currentTime = 0;
-    video.pause();
-}
